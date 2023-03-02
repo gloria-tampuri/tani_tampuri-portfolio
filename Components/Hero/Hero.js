5f7c66154ae7399810523087a1caf921e69544eb
gloria-tampuri <gloriatampuri15@gmail.com>
2023-03-02 15:17:06 +0000

    analytics

--- Components/Hero/Hero.js
+++ Components/Hero/Hero.js
@@ -1,24 +1,27 @@
 import React from 'react'
 import classes from './Hero.module.css'
+import gloria from '/public/icons/glow.jpg'
 import Typewriter from 'typewriter-effect'
+import Image from 'next/image'
 
 const Hero = () => {
     return (
         <div className={classes.Hero}>
 
-          <div className={classes.placement}>  <div className={classes.image}>
-
-</div>
-<div className={classes.brief}>
-    <h1>Hi, I am <span className={classes.tani}>
-        <Typewriter className={classes.type} options={{
-            strings:['Tani','Tampuri.'],
-            autoStart: true,
-            loop:true
-        }}/>
-        </span></h1>
-    <p>A Frontend developer with a passion for solving problems by creating immersive user experience on software and websites which are optimized for any device. I have a strong eye for innovative design and an in-depth understanding of techniques geared towards optimum user experience. I am constantly learning to keep up with the ever evolving tech community.</p>
-</div></div>
+            <div className={classes.placement}>
+                <div className={classes.image}>
+                    <Image className={classes.img} src={gloria} alt='gloria' width={200} height={200}/>
+                </div>
+                <div className={classes.brief}>
+                    <h1>Hi, I am <span className={classes.tani}>
+                        <Typewriter className={classes.type} options={{
+                            strings: ['Tani', 'Tampuri.'],
+                            autoStart: true,
+                            loop: true
+                        }} />
+                    </span></h1>
+                    <p>A Frontend developer with a passion for solving problems by creating immersive user experience on software and websites which are optimized for any device. I have a strong eye for innovative design and an in-depth understanding of techniques geared towards optimum user experience. I am constantly learning to keep up with the ever evolving tech community.</p>
+                </div></div>
 
         </div>
     )
