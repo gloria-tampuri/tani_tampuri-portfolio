8c97167587bbee3c8b942edce8704c33eda4a03b
gloriatampuri <gloria.tampuri@amalitech.org>
2023-11-15 12:22:59 +0000

    contact

--- Components/Footer/Footer.js
+++ Components/Footer/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import classes from './Footer.module.css'
+import Link from 'next/link'
 
 const Footer = () => {
     const currentYear = new Date().getFullYear()
   return (
     <div className={classes.footer}>
             <p>Gloria Tani Tampuri &copy; {currentYear}</p>
-           
+           <p> <Link className={classes.link}  href='mailto:gloriatampuri15@gmail.com'>      gloriatampuri15@gmail.com</Link></p>
+  <p>
+  <Link className={classes.link} href='tel:+233209102541'>+233 20 910 2541</Link>
+    </p>           
     </div>
   )
 }
--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Link from 'next/link'
 import classes from './Header.module.css'
 import Image from 'next/image'
@@ -7,7 +7,8 @@ import linkedIn from '/public/icons/linkedIn.svg'
 import twitter from '/public/icons/twitter (2).svg'
 import instagram from '/public/icons/instagram (2).svg'
 import { motion } from 'framer-motion'
-
+import { IoMdContact } from "react-icons/io";
+import { MdEmail,MdCall } from "react-icons/md";
 
 // Framer motion
 const logoVariant = {
@@ -28,6 +29,12 @@ const logoVariant = {
 }
 const Header = () => {
 
+    const[contact,setContact]=useState(false)
+
+    const handleContact=()=>{
+      setContact(!contact)
+    }
+    
   return (
     <div className={classes.header} >
         <div className={classes.names}>
@@ -38,12 +45,21 @@ const Header = () => {
           className={classes.logo}>GTT</motion.h1>
           <h2 className={classes.name}>Gloria Tani Tampuri</h2>
         </div>
+        
         <div className={classes.socials}>
+          <div className={classes.handleContact}>
+        <IoMdContact className={classes.contact} onClick={handleContact}/>
+        {contact && <div className={classes.view}>
+          <p><MdEmail />gloriatampuri15@gmail.com</p>
+          <p> <MdCall />+233 20 910 2541</p>
+        </div>}
+          </div>
         <Link href="https://github.com/gloria-tampuri">  <Image src={git} alt='github' width={25} height={25}/></Link>
          <Link  href='https://www.linkedin.com/in/gloria-tampuri-6ab397157'> <Image className={classes.social} src={linkedIn} alt='LinkedIn' width={25} height={25}/></Link>
           <Link  href='https://twitter.com/glow_tani'><Image className={classes.social} src={twitter} alt='twitter' width={25} height={25}/></Link>
          <Link  href="https://instagram.com/just_tani?igshid=YmMyMTA2M2Y="> <Image className={classes.social} src={instagram} alt='instagram' width={25} height={25}/></Link>
         </div>
+
     </div>
   )
 }
