73334ce0e9e14d71fd642c8faad9feec606bbf44
gloriatampuri <gloria.tampuri@amalitech.org>
2023-11-15 12:25:52 +0000

    scroll effect

--- Components/Header/Header.js
+++ Components/Header/Header.js
@@ -1,67 +1,94 @@
-import React,{useState} from 'react'
-import Link from 'next/link'
-import classes from './Header.module.css'
-import Image from 'next/image'
-import git from '/public/icons/github.svg'
-import linkedIn from '/public/icons/linkedIn.svg'
-import twitter from '/public/icons/twitter (2).svg'
-import instagram from '/public/icons/instagram (2).svg'
-import { motion } from 'framer-motion'
-import { IoMdContact } from "react-icons/io";
-import { MdEmail,MdCall } from "react-icons/md";
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
+import classes from './Header.module.css';
+import Image from 'next/image';
+import git from '/public/icons/github.svg';
+import linkedIn from '/public/icons/linkedIn.svg';
+import twitter from '/public/icons/twitter (2).svg';
+import instagram from '/public/icons/instagram (2).svg';
+import { motion } from 'framer-motion';
+import { IoMdContact } from 'react-icons/io';
+import { MdEmail, MdCall } from 'react-icons/md';
 
-// Framer motion
 const logoVariant = {
-  hidden:{
-    y: "-100px",
-    opacity: 0
+  hidden: {
+    y: '-100px',
+    opacity: 0,
   },
-  show:{
+  show: {
     y: 0,
-    opacity: [-2,-1,0,1,2],
-    transition:{
+    opacity: [-2, -1, 0, 1, 2],
+    transition: {
       duration: 0.3,
-      type: "spring",
+      type: 'spring',
       damping: 10,
-      // stiffness: 100
-    }
-  }
-}
+    },
+  },
+};
+
 const Header = () => {
+  const [contact, setContact] = useState(false);
 
-    const[contact,setContact]=useState(false)
+  const handleContact = () => {
+    setContact(!contact);
+  };
 
-    const handleContact=()=>{
-      setContact(!contact)
-    }
-    
+  useEffect(() => {
+    const handleScroll = () => {
+      // Set the contact state to false when a scroll happens
+      setContact(false);
+    };
+
+    // Add the scroll event listener
+    window.addEventListener('scroll', handleScroll);
+
+    // Remove the event listener when the component is unmounted
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []); // Empty dependency array ensures that this effect runs once when the component mounts
+
   return (
-    <div className={classes.header} >
-        <div className={classes.names}>
-          <motion.h1 
-          variants={logoVariant}
-          initial="hidden"
-          animate="show"
-          className={classes.logo}>GTT</motion.h1>
-          <h2 className={classes.name}>Gloria Tani Tampuri</h2>
-        </div>
-        
-        <div className={classes.socials}>
-          <div className={classes.handleContact}>
-        <IoMdContact className={classes.contact} onClick={handleContact}/>
-        {contact && <div className={classes.view}>
-          <p><MdEmail />gloriatampuri15@gmail.com</p>
-          <p> <MdCall />+233 20 910 2541</p>
-        </div>}
-          </div>
-        <Link href="https://github.com/gloria-tampuri">  <Image src={git} alt='github' width={25} height={25}/></Link>
-         <Link  href='https://www.linkedin.com/in/gloria-tampuri-6ab397157'> <Image className={classes.social} src={linkedIn} alt='LinkedIn' width={25} height={25}/></Link>
-          <Link  href='https://twitter.com/glow_tani'><Image className={classes.social} src={twitter} alt='twitter' width={25} height={25}/></Link>
-         <Link  href="https://instagram.com/just_tani?igshid=YmMyMTA2M2Y="> <Image className={classes.social} src={instagram} alt='instagram' width={25} height={25}/></Link>
-        </div>
+    <div className={classes.header}>
+      <div className={classes.names}>
+        <motion.h1 variants={logoVariant} initial="hidden" animate="show" className={classes.logo}>
+          GTT
+        </motion.h1>
+        <h2 className={classes.name}>Gloria Tani Tampuri</h2>
+      </div>
 
+      <div className={classes.socials}>
+        <div className={classes.handleContact}>
+          <IoMdContact className={classes.contact} onClick={handleContact} />
+          {contact && (
+            <div className={classes.view}>
+              <p>
+                <MdEmail />
+                gloriatampuri15@gmail.com
+              </p>
+              <p>
+                {' '}
+                <MdCall />
+                +233 20 910 2541
+              </p>
+            </div>
+          )}
+        </div>
+        <Link href="https://github.com/gloria-tampuri">
+          <Image src={git} alt="github" width={25} height={25} />
+        </Link>
+        <Link href="https://www.linkedin.com/in/gloria-tampuri-6ab397157">
+          <Image className={classes.social} src={linkedIn} alt="LinkedIn" width={25} height={25} />
+        </Link>
+        <Link href="https://twitter.com/glow_tani">
+          <Image className={classes.social} src={twitter} alt="twitter" width={25} height={25} />
+        </Link>
+        <Link href="https://instagram.com/just_tani?igshid=YmMyMTA2M2Y=">
+          <Image className={classes.social} src={instagram} alt="instagram" width={25} height={25} />
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
